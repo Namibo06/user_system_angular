72602727abb89445fd1e82cb6266bdab0bcd62c6
Namibo06 <namibo654@gmail.com>
2024-03-11 17:41:23 -0300

    style:title

--- src/app/components/title/title/title.component.ts
+++ src/app/components/title/title/title.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrl: './title.component.sass'
+  styleUrls: ['./title.component.sass','./title.responsive.component.sass']
 })
 export class TitleComponent {
   @Input() title:string='';
