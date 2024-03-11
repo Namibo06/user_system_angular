9eae49aa49e5ea2a6b69a4bfc8e18768aba7d30a
Namibo06 <namibo654@gmail.com>
2024-03-11 16:28:41 -0300

    create:project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'index',
+    component:IndexComponent
+  },
+  {
+    path:'**',
+    component:IndexComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
