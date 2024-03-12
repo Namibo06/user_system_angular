329f47a3b33903187f34cf0564b1562dd1921cba
Namibo06 <namibo654@gmail.com>
2024-03-11 23:58:41 -0300

    build:create-user

--- src/app/pages/create-user/create-user/create-user.component.ts
+++ src/app/pages/create-user/create-user/create-user.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-user.component.sass','./create-user.responsive.component.sass']
 })
 export class CreateUserComponent {
+  /**attributes */
+  first_name:string='';
+  last_name:string='';
+  email:string='';
+  nivel_user:string='';
+  password:string='';
+  confirm_password:string='';
 
+  /**callback's */
+  callbackError:boolean=false;
+  messageError:string='';
+  callbackSucess:boolean=false;
+  messageSucess:string='';
+
+  constructor(){
+
+  }
+
+  create_user(){
+    if(this.password !== this.confirm_password){
+      this.messageError="Senhas não são iguais";
+      this.callbackError=true;
+
+      setTimeout(() => {
+        this.callbackError=false;
+      }, 5000);
+    }
+  }
 }
